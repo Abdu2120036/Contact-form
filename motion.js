8715331155af843e76622b5c57c9411474e638a7
Abdulrahman Ben Omar <abdulrahmanbenomar@gmail.com>
2024-06-25 11:26:46 +0200

    Added the DOM

---
+++ motion.js
@@ -0,0 +1,11 @@
+const Fname = document.getElementById("Fname");
+const Lname = document.getElementById("Lname");
+const Email = document.getElementById("Email");
+const Genquiry = document.getElementById("Genquiry");
+const Srequest = document.getElementById("Srequest");
+const Message = document.getElementById("Message");
+const Consent = document.getElementById("Consent");
+const Submit = document.getElementById("Submit");
+
+const leftGeneral = document.querySelector(".sect3 .left");
+const rightSupport = document.querySelector(".sect3 .right");
