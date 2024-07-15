d5b6a258328c963f686dff1a9b0d386ec0aee55b
Abdulrahman Ben Omar <abdulrahmanbenomar@gmail.com>
2024-07-15 11:52:55 +0200

    Adjusted the code
    Due to better insight

--- motion.js
+++ motion.js
@@ -1,11 +1,19 @@
-const Fname = document.getElementById("Fname");
-const Lname = document.getElementById("Lname");
-const Email = document.getElementById("Email");
-const Genquiry = document.getElementById("Genquiry");
-const Srequest = document.getElementById("Srequest");
-const Message = document.getElementById("Message");
-const Consent = document.getElementById("Consent");
-const Submit = document.getElementById("Submit");
+const form = document.querySelector("form");
+const successMessage = document.getElementById('success-message');
 
-const leftGeneral = document.querySelector(".sect3 .left");
-const rightSupport = document.querySelector(".sect3 .right");
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const Fname = document.getElementById("Fname");
+    const Lname = document.getElementById("Lname");
+    const Email = document.getElementById("Email");
+    const Genquiry = document.getElementById("Genquiry");
+    const Srequest = document.getElementById("Srequest");
+    const Message = document.getElementById("Message");
+    const Consent = document.getElementById("Consent");
+    const Submit = document.getElementById("Submit");
+
+    const leftGeneral = document.querySelector(".sect3 .left");
+    const rightSupport = document.querySelector(".sect3 .right");
+
+})
